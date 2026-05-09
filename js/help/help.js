3f61c934eebb053314dedcf7cbccd4a24c981006
Prothes Barai <aspprothesbarai1619@gmail.com>
2026-05-09 15:57:33 +0600

    asp v44332

--- js/help/help.js
+++ js/help/help.js
@@ -1,7 +1,8 @@
 let categories = [];
 let currentItems = [];
 // U2hyZXlhc2lQcm90aGVzQW5na2Fu
-const lockedCategories = { "Cyber Security": "NzQyNjcy", "Bat Files": "NzQyNjcy", "GoLang": "R2F5YXRyaQ==","Vertual Box Setup": "NzQyNjcy", "KALI-Linux Command": "Sm95TWFLYWxp" };
+// Sm95TWFLYWxp
+const lockedCategories = { "Cyber Security": "NzQyNjcy", "Bat Files": "NzQyNjcy", "GoLang": "R2F5YXRyaQ==","Vertual Box Setup": "NzQyNjcy", "KALI-Linux Command": "" };
 let helperCDN_0x9a3f = "Li4vZG9jdW1lbnQuanNvbg==";
 //let helperCDN_0x9a3f = "aHR0cHM6Ly9wcm90aGVzYmFyYWkuZ2l0aHViLmlvL2NvbGxlY3QvSGVscC9kb2N1bWVudC5qc29u";
 
